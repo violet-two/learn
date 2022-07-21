54e816176685ae16087d45afba8f076c1552d619
violet-two <820918859@qq.com>
2022-07-21 16:21:43 +0800

    1

---
+++ network/src/main/java/com/example/network/RetrofitActivity.java
@@ -0,0 +1,19 @@
+package com.example.network;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RetrofitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit);
+    }
+
+    public void getRequest(View view) {
+
+    }
+}
