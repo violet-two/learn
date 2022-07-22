68b16103963be576e5724fd404981c35c97d18f3
violet-two <820918859@qq.com>
2022-07-22 11:39:26 +0800

    1

--- io/src/main/java/ws/com/io/MatrixActivity.java
+++ io/src/main/java/ws/com/io/MatrixActivity.java
@@ -11,7 +11,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
-public class MatrixActivity extends AppCompatActivity{
+public class MatrixActivity extends AppCompatActivity {
 
     private GestureDetector detector;
     private float currentScale;
@@ -22,44 +22,57 @@ public class MatrixActivity extends AppCompatActivity{
     private int height;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix);
-        detector = new GestureDetector(this,new GestureDetector.SimpleOnGestureListener(){
+        detector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                float vx = velocityX>4000?-4000f:velocityX;
-                vx = velocityX<-4000?-4000f:velocityX;
-
-                currentScale += currentScale *vx/4000.0f;
-                currentScale = currentScale>0.01?currentScale:0.01f;
-
+                float vx = velocityX > 4000 ? -4000f : velocityX;
+                vx = velocityX < -4000 ? -4000f : velocityX;
+                //根据手势的速度来计算缩放比例，如果vx>0,则放大图片，否则就缩小
+                currentScale += currentScale * vx / 4000.0f;
+                //保证不会小于0
+                currentScale = currentScale > 0.01 ? currentScale : 0.01f;
+                //重置matrix
                 matrix.reset();
-
-                matrix.setScale(currentScale,currentScale,160f,200f);
+                //缩放Matrix
+                matrix.setScale(currentScale, currentScale, 160f, 200f);
                 BitmapDrawable tmp = (BitmapDrawable) imageView.getDrawable();
-
-                if(!tmp.getBitmap().isRecycled()){
+                //回收图片
+                if (!tmp.getBitmap().isRecycled()) {
                     tmp.getBitmap().recycle();
                 }
-
-                Bitmap bitmap2 = Bitmap.createBitmap(bitmap,0,0,width,height,matrix,true);
+                //根据原始图位和matrix创建心图片
+                /*
+                x int 子位图第一个像素在源位图的X坐标
+                y int    子位图第一个像素在源位图的y坐标
+                width int     子位图每一行的像素个数
+                height int    子位图的行数
+                m  Matrix     对像素值进行变换的可选矩阵
+                filter boolean    如果为true，源图要被过滤。该参数仅在matrix包含了超过一个翻转才有效
+                Returns Bitmap    一个描述了源图指定子集的位图
+                */
+                Bitmap bitmap2 = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
                 imageView.setImageBitmap(bitmap2);
-
                 return true;
             }
         });
 
         imageView = findViewById(R.id.show);
         matrix = new Matrix();
-
+        //获取源图片
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.a);
+        //获得为图宽
         width = bitmap.getWidth();
+        //获得位图高
         height = bitmap.getHeight();
-        imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.a));
+        //设置imageView初始化时显示的图片
+        imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.a));
     }
+
     @Override
-    public boolean onTouchEvent(MotionEvent me){
+    public boolean onTouchEvent(MotionEvent me) {
         return detector.onTouchEvent(me);
     }
 }
