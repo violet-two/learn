ae07584a49df5a9aacf3e3276e05c485ba978c8e
violet-two <820918859@qq.com>
2022-07-22 17:59:48 +0800

    1

---
+++ hot-restart/src/main/java/ws/com/hot_restart/MainActivity.java
@@ -0,0 +1,14 @@
+package ws.com.hot_restart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
