baac61b9395311f818e3ff9f4eb6798e8cfc0514
violet-two <820918859@qq.com>
2022-08-12 17:58:05 +0800

    8.12

--- ui_control/src/main/java/com/example/ui_control/customView/SurfaceViewActivity.java
+++ ui_control/src/main/java/com/example/ui_control/customView/SurfaceViewActivity.java
@@ -3,14 +3,33 @@ package com.example.ui_control.customView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
 
 import com.example.ui_control.R;
 
-public class SurfaceViewActivity extends AppCompatActivity {
+public class SurfaceViewActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_surface_view);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
